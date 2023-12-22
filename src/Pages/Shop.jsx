45565a8c6ee8e9195a2dc95df839f29d6c208151
mhmtkarakas <m.karakas32@gmail.com>
2023-12-22 23:12:16 +0300

    components and pages created

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import Popular from "../Components/Popular/Popular";
+import Hero from "./../Components/Hero/Hero";
 
 const Shop = () => {
   return (
     <div>
-      
+      <Hero />
+      <Popular />
     </div>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
