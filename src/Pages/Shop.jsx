dbbfe56f0cb2aa7b9cffaadd9a04569ec6d73e67
mhmtkarakas <m.karakas32@gmail.com>
2023-12-23 20:55:51 +0300

    new components created and completed

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import NewCollections from "../Components/NewCollections/NewCollections";
+import Offers from "../Components/Offers/Offers";
 import Popular from "../Components/Popular/Popular";
 import Hero from "./../Components/Hero/Hero";
+import NewsLetter from "../Components/NewsLetter.jsx/NewsLetter";
+
 
 const Shop = () => {
   return (
     <div>
       <Hero />
       <Popular />
+      <Offers />
+      <NewCollections />
+      <NewsLetter />
+     
     </div>
   );
 };
