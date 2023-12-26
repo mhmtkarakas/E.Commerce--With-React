5ce4596f694b3c24c969fb2d658a3603d2110ce7
mhmtkarakas <m.karakas32@gmail.com>
2023-12-26 22:47:58 +0300

    Cart transactions completed

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './ProductDisplay.css'
 import star_icon from '../Assets/star_icon.png';
 import star_dull_icon from '../Assets/star_dull_icon.png';
+import { ShopContext } from './../../Context/ShopContext';
 
 const ProductDisplay = (props) => {
     const { product} = props;
+    const {addToCart} = useContext(ShopContext);
   return (
     <div className='productdisplay'>
         <div className="productdisplay-left">
@@ -38,14 +40,14 @@ const ProductDisplay = (props) => {
                 <div className="productdisplay-right-size">
                     <h1>Select Size</h1>
                     <div className="productdisplay-right-sizes">
-                        <div>S</div>
-                        <div>M</div>
-                        <div>L</div>
-                        <div>XL</div>
-                        <div>XXL</div>
+                        <div className='productdisplay-right-sizes-btn'>S</div>
+                        <div className='productdisplay-right-sizes-btn'>M</div>
+                        <div className='productdisplay-right-sizes-btn'>L</div>
+                        <div className='productdisplay-right-sizes-btn'>XL</div>
+                        <div className='productdisplay-right-sizes-btn'>XXL</div>
                     </div>
                 </div>
-                <button>ADD TO CART</button>
+                <button onClick={()=>addToCart(product.id)}>ADD TO CART</button>
                 <p className='productdisplay-right-category'><span>Category :</span>Women, T-Shirt, Crop Top</p>
                 <p className='productdisplay-right-category'><span>Tags :</span>Modern, Latest</p>
 
